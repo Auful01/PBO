84663f82429539e2b9b51fb04a7534c93c574cc9
Auful01 <auful@gmail.com>
2020-11-07 11:14:21 +0700

    update Jobsheet9

---
+++ Jobsheet9/Praktikum/Ikan.java
@@ -0,0 +1,9 @@
+/**
+ * Ikan
+ */
+public class Ikan extends Hewan{
+
+    public void bergerak() {
+            System.out.println("Berenang dengan SIRIP, \"wush..wush..\"");
+    }
+}
---
+++ Jobsheet9/Praktikum/Kucing.java
@@ -0,0 +1,11 @@
+/**
+ * Kucing
+ */
+public class Kucing extends Hewan{
+
+    @Override
+    public void bergerak() {
+            System.out.println("Bergerak dengan KAKI, \"tap..tap..tap..\"");
+    }
+    
+}
